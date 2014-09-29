7c589fd59dc10fc8a8df7dccca1f2fa0f7f3b9a1
Dorian Peake <dorian.peake@cantab.net>
2014-09-30 00:07:36 +0100

    Changed array from pass by value to pass by reference, playing with java has made me think everything is a reference already...

--- C++/sorting/BubbleSorter.h
+++ C++/sorting/BubbleSorter.h
@@ -1,13 +1,15 @@
 // BubbleSorter.cc
 // Sorter that implements the bubble sort
 
+#pragma once
+
 #include "Sorter.h"
 
 template <typename T>
 class BubbleSorter : public Sorter<T>
 {
     public:
-    virtual void sort( std::vector<T> data ) override
+    virtual void sort( std::vector<T> &data ) override
     {
         int length = data.size();
         for ( int i = 0; i < length; i++ )
